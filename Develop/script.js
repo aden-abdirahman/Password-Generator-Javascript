b811f9813f22c7036adf6dc38bee75b16f6d60b1
Abdirahman Aden <aden.abdirahman45@gmail.com>
2022-02-03 01:32:56 -0500

    finalizing code files and fixing bugs

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,25 @@
-// The Password generator will provide a password with 8-128 characters based on user preferences.
+// The Password generator will provide a password with 8-128 characters based on users preferences.
+
 document.querySelector("#generate").addEventListener("click", writePassword);
 
 // Variables
-let confirmLength = "";
-let confirmUpperCase;
-let confirmLowerCase;
-let confirmNumericalCharacter;
-let confirmSpecialCharacter;
+var confirmLength = "";
+var confirmUpperCase;
+var confirmLowerCase;
+var confirmNumericCharacter;
+var confirmSpecialCharacter;
 
 // Arrays 
-let number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-let alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-let alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+
 
 
 // password character length confirmation prompt
 function generatePassword() {
-  let confirmLength = (prompt("How many characters would you like your password to contain?"));
+  var confirmLength = (prompt("How many characters would you like your password to contain?"));
 
   // Loop if answer is outside the parameters 
   while(confirmLength <= 7 || confirmLength >= 129) {
@@ -25,31 +27,31 @@ function generatePassword() {
       var confirmLength = (prompt("How many characters would you like your password to contain?"));
       } 
 
-      // alert to show how many characters that password will contain 
+      // alert to show how many characters that password will contain  
       alert(`Your password will have ${confirmLength} characters`);
 
-    // sets parameters of password 
-    let confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
-    let confirmNumericalCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
-    let confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
-    let confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
+    // sets password parameters
+    var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
+    var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
+    var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
+    var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
       // Loop if answer is outside the parameters 
-      while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericalCharacter === false) {
+      while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
         alert("You must choose at least one parameter");
         var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
-        var confirmNumericalCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
+        var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
         var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
         var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");   
     } 
 
-      
+     
       var passwordCharacters = []
       
     if (confirmSpecialCharacter) {
       passwordCharacters = passwordCharacters.concat(specialChar)
     }
 
-    if (confirmNumericalCharacter) {
+    if (confirmNumericCharacter) {
       passwordCharacters = passwordCharacters.concat(number)
     }
       
